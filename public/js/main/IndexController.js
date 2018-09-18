f00b205615b0043cccd2828a17985676cccf717c
Cindy Muga <achiengcindy36@gmail.com>
2018-09-18 09:41:43 +0300

    registering service worker

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -8,7 +8,17 @@ export default function IndexController(container) {
   this._toastsView = new ToastsView(this._container);
   this._lostConnectionToast = null;
   this._openSocket();
+  this._registerServiceWorker();
 }
+
+IndexController.prototype._registerServiceWorker = function() {
+
+  if (!navigator.serviceWorker) return;
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log('service worker registered') }).catch(function(e){
+      console.log('reg failed')
+    });
+};
 
 // open a connection to the server for live updates
 IndexController.prototype._openSocket = function() {
